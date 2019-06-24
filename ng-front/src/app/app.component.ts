8aea61a665375b0fa846dbdc59a633eb341d799b
Elias <elias3425@gmail.com>
2019-06-24 14:14:41 +0200

    hide firebase config from git

--- ng-front/src/app/app.component.ts
+++ ng-front/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as firebase from 'firebase';
+import { firebaseConfig } from './firebase-config';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,7 @@ import * as firebase from 'firebase';
 export class AppComponent {
 
   constructor(){
-    const firebaseConfig = {
-      apiKey: 'AIzaSyAcTWRFE4tV9wi43C-B0UwDtJyhxq4atRs',
-      authDomain: 'blog5g.firebaseapp.com',
-      databaseURL: 'https://blog5g.firebaseio.com',
-      projectId: 'blog5g',
-      storageBucket: '',
-      messagingSenderId: '1068744934612',
-      appId: '1:1068744934612:web:fd956b78f33c99ce'
-    };
+
     firebase.initializeApp(firebaseConfig);
   }
 }
