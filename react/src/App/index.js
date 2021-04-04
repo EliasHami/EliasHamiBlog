71a44b6bd06c36f1e6949a16d6d127fab5d00fa8
Elias <elias3425@gmail.com>
2021-04-04 13:54:10 +0200

    clean firebase

--- react/src/App/index.js
+++ react/src/App/index.js
@@ -3,32 +3,27 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  // Link
 } from "react-router-dom";
-// import Firebase from 'firebase'
-// import firebaseConfig from '../firebase-config'
 import './style.css';
-// import Header from '../header'
+import Header from '../header'
 import Nav from '../nav'
-// import LandingPage from '../landingPage'
+import LandingPage from '../landingPage'
 import Footer from '../footer'
 import Blog from "../blog"
 import Post from "../post"
 import FourOhFour from "../four-oh-four"
 
-// Firebase.initializeApp(firebaseConfig);
-
 function App() {
   return (
     <div className="App">
         <Router>
-            {/* <Route exact path="/" component={Header}/> */}
+            <Route exact path="/" component={Header}/>
             <Nav/>
-            {/* <Route exact path="/" component={LandingPage} /> */}
+            <Route exact path="/" component={LandingPage} />
             <Switch>
-              <Route exact path="/" component={Blog}/>
+              {/* <Route exact path="/" component={Blog}/> */}
                 {/* <Blog firebase={Firebase} /> */}
-              <Route exact path="/posts/:id" component={Post}/>
+              {/* <Route exact path="/posts/:id" component={Post}/> */}
                 {/* <Post firebase={Firebase} /> */}
               <Route component={FourOhFour} />
             </Switch>
--- react/src/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export default {
-      apiKey: "AIzaSyAcTWRFE4tV9wi43C-B0UwDtJyhxq4atRs",
-      authDomain: "blog5g.firebaseapp.com",
-      databaseURL: "https://blog5g.firebaseio.com",
-      projectId: "blog5g",
-      storageBucket: "blog5g.appspot.com",
-      messagingSenderId: "1068744934612",
-      appId: "1:1068744934612:web:fd956b78f33c99ce"
-    };
